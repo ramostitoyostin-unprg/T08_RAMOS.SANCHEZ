a3ca26c78a5d45c4a0dd22028f0e8f9d88f65700
ramos tito <yramost@unprg.edu.pe>
2019-12-13 06:40:58 -0500

    se agrego el tercer ejercicio de manipuladores de texto

---
+++ sanches_manipuladores_textos/concatenacion..py
@@ -0,0 +1,134 @@
+# CONCATENACION 1:
+animal="leon"
+mc_kg=130
+prom_vida=14
+
+# agrupacion de cadenas
+mensaje1=("el " + animal + " es el rey de la selva, su peso es de " + str(mc_kg) + "kg y su promedio de vida es de " + str(prom_vida) + " años")
+print(mensaje1)
+
+# CONCATENACION 2:
+nombre="claudio"
+edad=15
+colegio="Pedro Abel Labathe Durand"
+
+# agrupacion de cadenas
+mensaje2=("el estudiante " + nombre + " tiene " + str(edad) + " años" + " y estudia en el colegio " + colegio)
+print(mensaje2)
+
+# CONCATENACION 3:
+instr="guitarra"
+num_cuerdas=6
+
+# agrupacion de cadenas
+mensaje3=("la " + instr + " es un instrumento de cuerda pulsada y tiene " + str(num_cuerdas) + " cuerdas")
+print(mensaje3)
+
+# CONCATENACION 4:
+est="elton jhonel"
+edad=20
+carr="INGENIERIA ELECTRONICA"
+esc="FACFYM"
+
+# agrupacion de cadenas
+mensaje4=("el estudiante " + est + " de " + str(edad) + " años estudia" + " la carrera de " + carr + " en la escuela de " + esc)
+print(mensaje4)
+
+# CONCATENACION 5:
+est="MAURICIO"
+edad=27
+ciclo=5
+esc="FACEAC"
+
+# agrupacion de cadenas
+mensaje5=("el estudiante " + est + " de " + str(edad) + " años" + " pertenece en el ciclo " + str(ciclo) + " en la escuela de " + esc)
+print(mensaje5)
+
+# CONCATENACION 6:
+est="RODRYGO"
+edad=18
+ciclo=8
+esc="FIA"
+
+# agrupacion de cadenas
+mensaje6=("el estudiante " + est + " de " + str(edad) + " años" + " pertenece en el ciclo " + str(ciclo) + " en la escuela de " + esc)
+print(mensaje6)
+
+# CONCATENACION 7:
+est="DIANA"
+edad=25
+ciclo=7
+carr="DERECHO"
+
+# agrupacion de cadenas
+mensaje7=("la estudiante " + est + " de " + str(edad) + " años" + " pertenece en el ciclo " + str(ciclo) + " y estudia la carrera de " + carr)
+print(mensaje7)
+
+# CONCATENACION 8:
+est="BRUNO"
+edad=30
+ciclo=9
+esc="INGENIERIA CIVIL"
+
+# agrupacion de cadenas
+mensaje8=("el estudiante " + est + " de " + str(edad) + " años" + " pertenece en el ciclo " + str(ciclo) + " y estudia la carrera de " + esc)
+print(mensaje8)
+
+# CONCATENACION 9:
+est="GABRIELA"
+edad=19
+ciclo=3
+esc="ENFERMERIA"
+
+# agrupacion de cadenas
+mensaje9=("la estudiante " + est + " de " + str(edad) + " años" + " pertenece en el ciclo " + str(ciclo) + " y estudia la carrera de " + esc)
+print(mensaje9)
+
+# CONCATENACION 10:
+est="PATRICIO"
+edad=32
+ciclo=10
+esc="BIOLOGIA"
+
+# agrupacion de cadenas
+mensaje10=("el estudiante " + est + " de " + str(edad) + " años" + " pertenece en el ciclo " + str(ciclo) + " y estudia la carrera de " + esc)
+print(mensaje10)
+
+# CONCATENACION 11:
+cient="STEPHEN HAWKING"
+pais="INGLATERRA"
+ofic="fisico"
+
+dato=("el cientifico " + cient + " nacio en " + pais + " y era un gran " + ofic)
+print(dato)
+
+# CONCATENACION 12:
+nombre="BILL GATES"
+edad=64
+nacion="ESTADOS UNIDOS"
+
+dato1=("el empresario informatico " + nombre + " de " + str(edad) + " años " + "nacio en " + nacion + " y es cofundador de software microsoft")
+print(dato1)
+
+# CONCATENACION 13:
+oper="25 + 28"
+numeros="naturales"
+
+dato2=("la suma de " + "los numeros " + numeros + " " + oper + " = " + "53")
+print(dato2)
+
+# CONCATENACION 14:
+oper="15.8 + 18.6"
+numeros="decimales"
+
+dato3=("la suma de la operacion " + "de numeros " + numeros + " " + oper + " = " + "34.4")
+print(dato3)
+
+# CONCATENACION 15:
+numero1=48
+numero2=6
+numero="naturales"
+
+dato4=("la multiplicacion " + "de los numeros " + numero + " " + str(numero1) + " x " + str(numero2) + " = " + "384")
+print(dato4)
+
