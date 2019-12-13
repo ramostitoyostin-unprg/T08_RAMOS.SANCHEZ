e88b9078e71d73c8a3b3ea6c0539d11e4bd486a2
ramostitoyostin-unprg <57698982+ramostitoyostin-unprg@users.noreply.github.com>
2019-12-12 23:53:44 -0500

    se agrego la primera operacion de cadenas

---
+++ manipuladores_de_textos/indexacion.py
@@ -0,0 +1,61 @@
+#MANIPULACION DE TEXTOS
+#                 10
+#       01234567890123456789
+
+cadena="HOY TE IRÁ MUY BIEN"
+
+#manipulador de texto nro 1
+print(cadena[5],cadena[6],cadena[12])   #me imprime los  caracteres "E  I" que ocupa los indice 5 6 12
+
+#manipulador de texto nro 2
+print(cadena[2],cadena[9])    #me imprime los caracteres "Y Á " que ocupa los indices 2 9
+
+#manipulador de texto nro 3
+print(cadena[4])   #me imprime el caracter "T" que ocupa el indice 4                                         que ocupa el indice 2
+
+#manipulador de texto nro 4
+print(cadena[1],cadena[6])   #me imprime el caracteres "O " que ocupa los indices 1 6
+
+#manipulador de texto nro 5
+print(cadena[0],cadena[5],cadena[8],cadena[15])#me imprime los caracter "H E R B" que ocupa los indice 0 5 8 15
+
+#manipulador de texto nro 6
+print(cadena[9],cadena[17],cadena[11])   #me imprime los caracter "A E M" que ocupa los indices 9 17 11
+
+#manipulador de texto nro 7
+print(cadena[11])   #me imprime el caracter "M " que ocupa el indice 11
+
+#manipulador de texto nro 8
+print(cadena[10])   #me imprime el caracter " " que ocupa el indice 10
+
+#manipulador de texto nro 9
+print(cadena[18],cadena,[7],cadena[0])   #me imprime el caracter "N I O" que ocupa los indice 18 7 0
+
+#manipulador de texto nro 10
+print(cadena[16,cadena[11]])   #me imprime el caracter "I M" que ocupa los indice 16 11
+
+#manipulador de texto nro 11
+print(cadena[7],cadena[5],cadena,[15])   #me imprime los caracteres "I E B" que ocupa los indice 7 5 15
+
+#manipulador de texto nro 12
+print(cadena[8])   #me imprime el caracter "R" que ocupa el indice 8
+
+#manipulador de texto nro 13
+print(cadena[17],cadena[5])   #me imprime los caracteres "E E" que ocupa los indices 17 5
+
+#manipulador de texto nro 14
+print(cadena[14])   #me imprime el caracter " " que ocupa el indice 14
+
+#manipulador de texto nro 15
+print(cadena[13],cadena[8],cadena[9])   #me imprime los caracteres "Y R A" que ocupa los indices 13
+
+
+
+
+
+
+
+
+
+
+
