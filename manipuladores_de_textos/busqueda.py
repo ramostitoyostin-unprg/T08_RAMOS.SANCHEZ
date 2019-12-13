83d0dd4c37774e2a75996b523919158a67f5d863
ramos tito <yramost@unprg.edu.pe>
2019-12-13 00:06:37 -0500

    se agrego la septima operacion de cadenas

---
+++ manipuladores_de_textos/busqueda.py
@@ -0,0 +1,22 @@
+#OPERACION BUSQUEDA DE CADENAS
+
+#manipulador de texto nro1#
+#       0123456789
+cadena="hola mundo"
+print(cadena.index("n"))  # me mostrara el indice que ocupa el caracter ->n
+
+#manipulador de texto nro2#
+cad="QUIEN QUIERE VIAJAR CONMIGO"
+print(cad.find("QUIERE"))
+
+#manipulador de texto nro3#
+cad1="EL MUNDO ES UN LIBRO"
+print(cad1.index("LIBRO"))
+
+#manipulador de texto nro4#
+cad1="EL MUNDO ES UN LIBRO"
+print(cad1.index("EL"))
+
+#manipulador de texto nro5#
+cad1="EL MUNDO ES UN LIBRO"
+print(cad1.find("MUNDO"))
