3245f2ea2dda963ba1491a7e51363300b7b16b77
ramos tito <yramost@unprg.edu.pe>
2019-12-13 06:37:43 -0500

    se agrego e primer ejercicio de manipuladores de texto

---
+++ sanches_manipuladores_textos/indexacion..py
@@ -0,0 +1,127 @@
+# INDEXACION 1:
+# una escuela de la universidad
+#    0         10        20        30
+#    012345678901234567890123456789012
+esc="ESCUELA DE INGENIERIA ELECTRONICA"
+
+# pais= esc[18] + esc[3] + esc[1] + esc[19] + esc[20]
+print(esc[18], esc[3], esc[1], esc[19], esc[20])
+
+# INDEXACION 2:
+# un poema
+#      0         1         2         3         4         5         6
+#      0123456789012345678901234567890123456789012345678901234567890123456
+pma="EN LA PUERTA DE MI CASA PLANTE DOS ARBOLES UN TE QUIERO Y UN TE AMO"
+
+# nombres= patricia, elton
+print(pma[6] + " " + pma[20] + " " + pma[10] + " " + pma[36] + " " + pma[51] + " " + pma[19] + " " + pma[17] + " " + pma[35] + ", " + " " + pma[8] + " " + pma[25] + " " + pma[46] + " " + pma[66] + " " + pma[59])
+
+# INDEXACAION 3:
+# frase de Albert Einstein
+#    0         1         2         3         4         5
+#    0123456789012345678901234567890123456789012345678901234
+fse="LA DEBILIDAD DE ACTITUD SE VUELVE DEBILIDAD DE CARACTER"
+
+# animal= RATA
+print(fse[49] + " " + fse[16] + " " + fse[20] + " " + fse[41])
+
+# INDEXACION 4:
+#      0         1         2
+#      01234567890123456789012
+frase="UNO ES EL QUE ELIGE SER"
+
+# Q U E S O
+print(frase[10], frase[0], frase[4], frase[20], frase[2])
+
+# INDEXACION 5:
+#    0         1         2
+#    012345678901234567890
+msg="RIE HOY, LLORA MAÑANA"
+
+# palabra= AMOR
+print(msg[-1], msg[-6], msg[-10], msg[-21])
+
+# INDEXACION 6:
+#     0         1
+#     01234567890123
+msg1="COMO TE LLAMAS"
+
+# APLELIDO= TOMAS
+print(msg1[5], msg1[1], msg1[11], msg1[12], msg1[13])
+
+# INDEXACION 7:
+#     0         1
+#     0123456789012345687
+msg2="CUANTOS AÑOS TIENES"
+
+# nombre= A N A
+print(msg2[2], msg2[16], msg2[8])
+
+# INDEXACION 8:
+#     0         1
+#     01234567890123
+msg3="ASI ES LA VIDA"
+
+# nombre= ELSA
+print(msg3[4], msg3[7], msg3[1], msg3[13])
+
+# INDEXACION 9:
+#     0         1         2         3         4
+#     012345678901234567890123456789012345678901
+msg4="FACULTAD DE CIENCIAS FISICAS Y MATEMATICAS"
+
+print(msg4[21], msg4[1], msg4[12], msg4[21], msg4[29], msg4[31])
+
+# INDEXACION 10:
+# mensaje
+#     0         1
+#     01234567890123456789
+msg5="SI NO SUMA QUE RESTE"
+
+# S A R A
+print(msg5[0], msg5[9], msg5[15], msg5[9])
+
+# INDEXACION 11:
+#     0         1
+#     0123456789012
+msg6="FELIZ NAVIDAD"
+
+# N A D I E
+print(msg6[6], msg6[11], msg6[12], msg6[9], msg6[1])
+
+# INDEXACION 12:
+#     0         1         2         3
+#     012345678901234567890123456789012
+msg7="SI PUEDES SOÑARLO, PUEDES HACERLO"
+
+# P A S A D O
+print(msg7[3], msg7[13], msg7[10], msg7[27], msg7[6], msg7[32])
+
+# INDEXACION 13:
+#     0         1         2         3
+#     01234567890123456789012345678901234
+msg8="LA VIDA ES MUY CORTA PARA ABURRIRSE"
+
+# B R A S I L
+print(msg8[27], msg8[29], msg8[19], msg8[9], msg8[31], msg8[0])
+
+# INDEXACION 14:
+#     0         1         2         3
+#     0123456789012345678901234567890
+msg9="YO SOLO SE QUE NADA QUE NADA SE"
+
+# A D A N
+print(msg9[16], msg9[26], msg9[25], msg9[24])
+
+# INDEXACION 15:
+#      0         1         2
+#      01234567890123456789012
+msg10="PROGRAMACION ES CHEVERE"
+
+# C H A V I N
+print(msg10[16], msg10[17], msg10[5], msg10[19], msg10[9], msg10[11])
+
+
+
+
+
