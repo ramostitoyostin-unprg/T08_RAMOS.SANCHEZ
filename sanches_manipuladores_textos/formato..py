3b40508f26f9a1bab32f404693bde422b9c7cb57
ramos tito <yramost@unprg.edu.pe>
2019-12-13 06:48:05 -0500

    se agrego el octavo ejercicio de manipuladores de texto

---
+++ sanches_manipuladores_textos/formato..py
@@ -0,0 +1,104 @@
+# FUNCIONES DE MANIPULACION DE CADENA 1:
+msg="EL FIN DEL MUNDO SE ACERCA"
+
+# Mostrar el nro de ocurrencias de la palabra FIN
+print("FIN",msg.count("FIN"))
+print("A ->", msg.count("A"))
+
+# FUNCIONES DE MANIPULACION DE CADENA 2:
+cadena1="LOS VENGADORES FIN DE LA GUERRA"
+
+# Transformar el texto en minusculas
+mensaje=cadena1.lower()
+print(mensaje)
+
+# FUNCIONES DE MANIPULACION DE CADENA 3:
+cadena2="para los niños existe papa noel"
+
+# Transformar en mayusculas
+mensaje=cadena2.upper()
+print(mensaje)
+
+# FUNCIONES DE MANIPULACION DE CADENA 4:
+cadena3="LA CAPITAL DE PErU ES LIMA"
+
+# transformar en minusculas
+mensaje=cadena3.lower()
+print(mensaje)
+
+# FUNCIONES DE MANIPULACION 5:
+cadena4="BAJO EL ARBOL ENCONTRE UNA MANZANA"
+
+# Mostrar el nro de ocurrencias de la palabra MANZANA
+print("MANZANA", cadena4.count("MANZANA"))
+print("N ->", cadena4.count("N"))
+
+# FUNCIONES DE MANIPULACION DE CADENA 6:
+cadena5="los gatos no nadan"
+
+# transformar en maysuculas
+mensaje=cadena5.upper()
+print(mensaje)
+
+# FUNCIONES DE MANIPULACION DE CADENA 7:
+cadena6="SUMMIT LA SUPERCOMPUTADORA MAS PODEROSA DEL MUNDO"
+
+# Mostrar el nro de ocurrencias de la palabra SUPERCOMPUTADORA
+print("SUPERCOMPUTADORA", cadena6.count("SUPERCOMPUTADORA"))
+print("E ->", cadena6.count("E"))
+
+# FUNCIONES DE MANIPULACION DE CADENA 8:
+cadena7="LOS GATOS ODIAN A LOS PERROS"
+
+# transformar en minusculas
+mensaje=cadena7.lower()
+print(mensaje)
+
+# FUNCIONES DE MANIPULACION DE CADENA 9:
+cadena8="los ricos tambien lloran"
+
+# transformar en mayusculas
+mensaje=cadena8.upper()
+print(mensaje)
+
+# FUNCIONES DE MANIPULACION DE CADENA 10:
+cadena10="TODO FLUYE, NADA PERMANECE"
+
+# transformar en minusculas
+mensaje=cadena10.lower()
+print(mensaje)
+
+# FUNCIONES DE MANIPULACION DE CADENA 11:
+cadena11="Si no esperas lo inesperado no lo reconocerás cuando llegue"
+
+# transformar en mayusculas
+mensaje=cadena11.upper()
+print(mensaje)
+
+# FUNCIONES DE MANIPULACION DE CADENA 12:
+cadena12="Los asnos prefieren la paja al oro"
+
+# transformar en mayusculas
+mensaje=cadena12.upper()
+print(mensaje)
+
+# FUNCIONES DE MANIPULACION DE CADENA 13:
+cadena13=" Todas las leyes humanas se alimentan de la ley divina"
+
+# transformar en mayusculas
+mensaje=cadena13.upper()
+print(mensaje)
+
+# FUNCIONES DE MANIPULACION DE CADENA 14:
+cadena14=" Muerte es todo lo que vemos despiertos; sueño lo que vemos dormidos"
+
+# Mostrar el nro de ocurrencias de la palabbra dormidos
+print("dormidos", cadena14.count("dormidos"))
+print("o ->", cadena14.count("o"))
+
+# FUNCIONES DE MANIPULACION DE CADENA 15:
+cadena15="LOS PERROS SOLO LADRAN A QUIENES NO CONOCEN"
+
+# transformar en minusculas
+mensaje=cadena15.lower()
+print(mensaje)
